71d4f8a1f0c507fbbbc44784673d710b668f5ce0
juess123 <cloudt020408@gmail.com>
2025-06-23 16:38:46 +0800

    hello

--- Core/main.c
+++ Core/main.c
@@ -3,35 +3,6 @@
 #include "../Inc/task.h"
 TaskHandle_t T1,T2,T3;
 extern int temp;
-void TestFunc1(void* parameters)
-{
-	uint32_t i=0;
-    while(1)
-	{
-		i++;
-		OLED_ShowNum(1,1,i,4);
-		
-		vTaskDelay(20);	
-	}
-}
-
-void TestFunc2(void* parameters)
-{
-	uint32_t i=0;
-	
-	while(1)
-    {
-		i++;
-        OLED_ShowHexNum(4,1,i,4);
-		
-        vTaskDelay(20);
-    } 
-}
-void HardFault_Handler(void)
-{
-	char a='A';
-	OLED_ShowHexNum(3,8,a,4);
-}
 int main(void)
 {
 	int a=0;
