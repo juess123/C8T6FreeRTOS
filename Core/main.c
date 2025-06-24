6d04b02516cb1c6b44c6d816c94383740896bb30
juess123 <cloudt020408@gmail.com>
2025-06-24 17:43:41 +0800

    e

--- Core/main.c
+++ Core/main.c
@@ -1,54 +1,53 @@
-
-
-#include "../Inc/event_groups.h"
+#include "../Inc/stm32f10x.h"
 #include "../Inc/OLED.h"
-#define BIT_0 (1 << 0)
-
-static EventGroupHandle_t xTestEventGroup = NULL;
-
-void vTaskA(void *pvParameters)
+void uart1_init(void)
 {
-    EventBits_t uxBits;
-    OLED_ShowChar(4,1,'o');
-    uxBits = xEventGroupWaitBits(
-        xTestEventGroup,  // 等待的事件组
-        BIT_0,            // 要等待的事件位
-        pdTRUE,           // 满足后清除事件位
-        pdTRUE,           // 等待所有位（这里只有 BIT_0）
-        5000 // 最多等 5 秒
-    );
-    if ((uxBits & BIT_0) != 0)
-    {
-       
-        OLED_ShowChar(1,1,'s');
-    }
-    else
-    {
-        OLED_ShowChar(2,1,'t');
-    }
-    while(1)
-    {
-        vTaskDelay(2000);
-    }
-}
+    RCC->APB2ENR |=(1<<2);
+    RCC->APB2ENR |=(1<<14);
 
-// 任务 B：延迟一段时间后设置 BIT_0
-void vTaskB(void *pvParameters)
-{           
-    EventBits_t a;
-    a=xEventGroupSetBits(xTestEventGroup, BIT_0);
-    while(1)
-    {
-        vTaskDelay(1000);
-    }
-}
+    GPIOA->CRH &= ~(0xf<<4);
+    GPIOA->CRH |= (0xb<<4);
 
+    GPIOA->CRH &= ~(0xF << 8);
+    GPIOA->CRH |=  (0x4 << 8); 
+
+    USART1->BRR =(468<<4)|12;
+
+    USART1->CR1 |=(1<<13);
+    USART1->CR1 |=(1<<3);
+    USART1->CR1 |= (1 << 2); 
+}
+void uart1_send_char(char c)
+{
+    while (!(USART1->SR & (1 << 7))); // 等待 TXE = 1，数据寄存器空
+    USART1->DR = c;
+}
+char uart1_recv_char(void)
+{
+    while (!(USART1->SR & (1 << 5))); // 等待 RXNE = 1
+    return USART1->DR;                // 读取接收到的数据
+}
+void HardFault_Handler(void)
+{
+    OLED_ShowChar(4,1,'e');
+}
 int main(void)
 {
     OLED_Init();
-    xTestEventGroup = xEventGroupCreate();
-    xTaskCreate(vTaskA, "TaskA", 256, NULL, 1, NULL);
-    xTaskCreate(vTaskB, "TaskB", 256, NULL, 2, NULL);
-    vTaskStartScheduler();
-    for (;;);
+    uart1_init();
+
+    
+    uint32_t i=0;
+    char p[16]={0};
+    char* s="Readyhessd";
+    char c =0;
+    while (*s)
+    {
+        uart1_send_char(*s++);
+        c = uart1_recv_char();
+        p[i++]=c;
+    }
+    OLED_ShowString(1,1,p);
+    while(1);
+   
 }
